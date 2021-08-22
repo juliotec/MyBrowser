8b0e6bb490068b52faaff2f85d007585a9e027cb
juliotec <juliotec2@hotmail.com>
2021-08-22 18:27:05 -0500

    Changes

--- MyBrowser/App.xaml.cs
+++ MyBrowser/App.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.Wpf;
 
 namespace MyBrowser
 {
@@ -13,5 +12,101 @@ namespace MyBrowser
     /// </summary>
     public partial class App : Application
     {
+        #region Propiedades
+
+        public static string[] DesktopUserAgents
+        {
+            get
+            {
+                if (_desktopUserAgents == null)
+                {
+                    _desktopUserAgents = new string[]
+                    {
+                        @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+                    };
+                }
+
+                return _desktopUserAgents;
+            }
+        }
+        private static string[] _desktopUserAgents;
+
+        public static Random Random
+        {
+            get
+            {
+                if (_random == null)
+                {
+                    _random = new Random();
+                }
+
+                return _random;
+            }
+        }
+        private static Random _random;
+
+        #endregion
+        #region Metodos
+
+        public static void CerrarCefSharpProcess()
+        {
+            var processes = Process.GetProcessesByName("CefSharp.BrowserSubprocess");
+
+            foreach (var process in processes)
+            {
+                try
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            DispatcherUnhandledException += (o, e2) =>
+            {
+                Shutdown();
+            };
+            AppDomain.CurrentDomain.UnhandledException += (o, e2) =>
+            {
+                Shutdown();
+            };
+            Dispatcher.UnhandledException += (o, e2) =>
+            {
+                Shutdown();
+            };
+            TaskScheduler.UnobservedTaskException += (o, e2) =>
+            {
+                Shutdown();
+            };
+            CerrarCefSharpProcess();
+
+            var cefsettings = new CefSettings
+            {
+                UserAgent = DesktopUserAgents[Random.Next(0, DesktopUserAgents.Length)]
+            };
+
+            _ = Cef.Initialize(cefsettings);
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            if (Cef.IsInitialized)
+            {
+                Cef.Shutdown();
+            }
+        }
+
+
+        #endregion
     }
 }
---
+++ MyBrowser/PrincipalWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace MyBrowser
+{
+    public partial class PrincipalWindow : Window
+    {
+        #region Campos
+
+        private ChromiumWebBrowser _browser;
+
+        #endregion
+        #region Constructores
+
+        public PrincipalWindow()
+        {
+            InitializeComponent();
+            IniciarBrowser();
+        }
+
+        #endregion
+        #region Metodos
+
+        private void IniciarBrowser()
+        {
+            _browser = new ChromiumWebBrowser()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalContentAlignment = VerticalAlignment.Stretch,
+                HorizontalContentAlignment = HorizontalAlignment.Stretch
+            };
+            _window.Content = _browser;
+        }
+
+        private void Limpiar()
+        {
+            if (_browser != null && !_browser.IsDisposed)
+            {
+                _browser.Stop();
+                _browser.Dispose();
+            }
+        }
+
+        private void PrincipalWindowClosed(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+
+        #endregion
+    }
+}
